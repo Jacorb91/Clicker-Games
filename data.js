3e3cca51dc8416e0b4ca745241c56917052047cf
Jacorb91 <124224543+Jacorb91@users.noreply.github.com>
2023-02-13 00:30:13 -0300

    hehe

--- data.js
+++ data.js
@@ -1,6 +1,10 @@
 localStorage.setItem("dollars", 0)
 var clicking = document.querySelector(".clicks")
 
+function clicksEarn() {
+    localStorage.dollars = Number(localStorage.dollars) 
+}
+
 setInterval(() => {
     clicking.innerHTML = "Your Haven " + localStorage.dollars + " Dollars."
 }, 100);
