dc838071b86ffc103f20bb2a9cd7b7644fbeaf1c
Jacorb91 <124224543+Jacorb91@users.noreply.github.com>
2023-02-13 09:33:18 -0300

    best

--- data.js
+++ data.js
@@ -1,8 +1,14 @@
 localStorage.setItem("dollars", 0)
+localStorage.setItem("dpc", 1)
 var clicking = document.querySelector(".clicks")
 
 function clicksEarn() {
-    localStorage.dollars = Number(localStorage.dollars) + 1;
+    localStorage.dollars = Number(localStorage.dollars) + Number(localStorage.dpc);
+}
+
+function buyCursor() {
+    localStorage.dollars = Number(localStorage.dollars) - 50;
+    localStorage.dpc = Number(localStorage.dpc) + 1;
 }
 
 setInterval(() => {
