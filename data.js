031d2cd85463514378afffc0dd0375f3b5568fee
Jacorb91 <124224543+Jacorb91@users.noreply.github.com>
2023-02-13 09:28:16 -0300

    increment

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ localStorage.setItem("dollars", 0)
 var clicking = document.querySelector(".clicks")
 
 function clicksEarn() {
-    localStorage.dollars = Number(localStorage.dollars) 
+    localStorage.dollars = Number(localStorage.dollars) + 1;
 }
 
 setInterval(() => {
